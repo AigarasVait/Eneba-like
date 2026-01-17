dd9b15102288437172f41be8552e2c76bb1e1e4a
AigarasVait <aigarasvaitkus@gmail.com>
2026-01-17 19:44:39 +0200

    base card

---
+++ frontend/src/components/ListingList/ListingCard.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./ListingCard.css"
+import { ListingCardDTO } from "../types";
+
+interface Props {
+  listing: ListingCardDTO;
+}
+
+const ListingCard: React.FC<Props> = ({ listing }) => {
+  return (
+    <div className="card">
+      <img src={listing.gameImagePath} alt={listing.name} className="cover" />
+      <div className="info">
+        <h3>{listing.name}</h3>
+        <p>{listing.platform} • {listing.region}</p>
+        <p className="price">
+          <span className="original">€{listing.basePrice.toFixed(2)}</span>
+          <span className="discount">-{listing.discountPercent}%</span>
+          <span className="final">€{listing.price.toFixed(2)}</span>
+        </p>
+        <p className="cashback">Cashback: €{listing.cashback.toFixed(2)}</p>
+        <p className="likes">❤️ {listing.favoritedCount}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ListingCard;
